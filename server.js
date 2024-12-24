47987e2f18a6c4ff1f8ac3e30bd654672b057674
sweetkitten27 <lukestot0103@gmail.com>
2024-12-23 17:02:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,18 @@ let intialPath = path.join(__dirname, "public");
 app.use(bodyParser.json());
 app.use(express.static(intialPath));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(intialPath, "index.html"));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(intialPath, "login.html"));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(intialPath, "register.html"));
+})
+
 app.post('/register-user', (req, res) => {
     const { name, email, password } = req.body;
 
